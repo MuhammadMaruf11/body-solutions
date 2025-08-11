4c98a7a3434a40da3066b0f2c68d85e4910c14cb
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2025-08-11 16:57:56 +0600

    chore: initial project setup with React + Tailwind + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Banner from "./components/Banner/Banner"
+import Footer from "./components/common/Footer"
+import Header from "./components/common/Header"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Footer />
+    </>
+  )
+}
+
+export default App
