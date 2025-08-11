9b825b06409fe0bbb0a7a2f7f6ba89efc28b94e6
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2025-08-11 21:36:45 +0600

    feat: header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import Banner from "./components/Banner/Banner"
 import Footer from "./components/common/Footer"
 import Header from "./components/common/Header"
+import Details from "./components/Details/Details"
+import Service from "./components/Service/Service"
+import Testimonial from "./components/Testimonial/Testimonial"
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
+      {/* Other components can be added here */}
+      <Service />
+      <Testimonial />
+      <Details />
       <Footer />
     </>
   )
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,17 @@
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header className="bg-[#FEEDBC] text-white py-15">
+            <div className="container mx-auto bg-white py-4 rounded-full relative text-center">
+                <div className="inline-block">
+                    <img src="/img/logo/logo.png" alt="logo" />
+                </div>
+                <div className="absolute top-1/2 -translate-y-1/2 right-7.5 flex gap-3">
+                    <img src="/img/icons/flag.png" alt="icon" />
+                    <img src="/img/icons/arrow_drop_down.svg" alt="icon" />
+                </div>
+            </div>
+        </header>
     )
 }
 
